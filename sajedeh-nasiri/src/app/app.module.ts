3658f4bbee96787997e6e753150f2fff37c3f37e
Behzad Chizari <Behzad Chizari@DESKTOP-44PV0ES>
2023-02-25 15:08:39 +0100

    undo

---
+++ sajedeh-nasiri/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { LeafyComponent } from './components/projects/leafy/leafy.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    ResumeComponent,
+    LeafyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule
+  ],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
